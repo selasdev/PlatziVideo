2457ccb98b83376b02f2a0850423391995ad5908
Selas <carlosros033@gmail.com>
2020-02-21 19:04:49 -0400

    agregando Link en diferentes componentes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,16 @@
 import React from "react";
-import '../assets/styles/components/Header.scss';
+import { Link } from "react-router-dom";
+import "../assets/styles/components/Header.scss";
 
-import logo from '../assets/static/logo-platzi-video-BW2.png';
-import userIcon from '../assets/static/user-icon.png';
+import logo from "../assets/static/logo-platzi-video-BW2.png";
+import userIcon from "../assets/static/user-icon.png";
 
 const Header = () => {
   return (
     <header className="header">
-      <img
-        className="header__img"
-        src={logo}
-        alt="Platzi Video"
-      />
+      <Link to="/">
+        <img className="header__img" src={logo} alt="Platzi Video" />
+      </Link>
       <div className="header__menu">
         <div className="header__menu--profile">
           <img src={userIcon} alt="" />
