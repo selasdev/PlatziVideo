d83723c021aeba38a9001eb2090735392852abc4
Selas <carlosros033@gmail.com>
2020-02-21 16:53:42 -0400

    Agregando el componente de Registro

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Home from '../containers/Home';
-import Login from '../containers/Login';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "../containers/Home";
+import Login from "../containers/Login";
+import Register from "../containers/Register";
 
 const App = () => (
-    <BrowserRouter>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-    </BrowserRouter>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+    </Switch>
+  </BrowserRouter>
 );
 
 export default App;
