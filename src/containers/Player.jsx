05f3e03008c317d76f171e7f9893787e8a4b8959
Selas <carlosros033@gmail.com>
2020-02-23 12:25:16 -0400

    conectando el register con redux

---
+++ src/containers/Player.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../assets/styles/components/Player.scss";
+
+const Player = () => {
+  return (
+    <div className="player">
+      <video controls autoPlay>
+        <source src="" type="video/mp4" />
+      </video>
+      <div className="Player-black">
+        <button type="button">Regresar</button>
+      </div>
+    </div>
+  );
+};
+
+export default Player;
